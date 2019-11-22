b572930bc42bafed79b25ce941427dc7c21934f0
Vlad <52528166+Vlad-code-c@users.noreply.github.com>
2019-11-23 00:55:47 +0200

    Added function for creating and deleting databases

---
+++ create.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+void createD()
+{
+    ofstream c("Results/Create.txt");
+    string result, str;
+
+    cout << "Sunteti pe cale de a crea o baza de date\n";
+    cout << "\nIntroduceti numele bazei de date: ";
+    cin >> str;
+
+    result = "--CREAREA BAZEI DE DATE"
+             "\nUSE master"
+             "\nGO"
+             "\nIF exists("
+             	"\n\tSELECT *FROM sys.databases WHERE name='" + str + "')"
+             "\nBEGIN"
+             		"\n\t\tALTER DATABASE " + str + " SET single_user"
+             		"\n\t\tWITH ROLLBACK IMMEDIATE"
+             		"\n\t\tDROP DATABASE " + str +
+             		"\n\t\tEND"
+             "\nGO"
+             "\nCREATE DATABASE " + str +
+             "\nGO"
+             "\nUSE " + str +
+                 "\nGO";
+
+    c << result;
+        cout << "Fisierul Create.sql se afla la locatia: ./Results/ \n";
+}
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "header.h"
+main()
+{
+    int n;
+    system("cls");
+    cout << "Alegeti una din optiunile: ";
+    cout << "\n1.Creati o baza de date";
+    cout << "\n2.Stergeti o baza de date";
+    cout << "\n3.Adaugati tabele baza de date";
+    cout << "\n4.Inserati date in baza de date\n";      //Create file with random files
+    cin >> n;
+    system("cls");
+    switch (n)
+    {
+    case 1:
+        createD();
+        break;
+    case 2:
+        deleteD();
+        break;
+    }
+
+}
