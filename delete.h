4a425cdbf4598a06e929dfca0f62c2a104fad558
Vlad-code-c <vladz1992b@gmail.com>
2019-11-23 16:23:04 +0200

    Added Random Data Files

---
+++ delete.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+deleteD(){
+
+ ofstream c("Results/delete.txt");
+    string result, str;
+
+    cout << "Sunteti pe cale de a sterge o baza de date\n";
+    cout << "\nIntroduceti numele bazei de date: ";
+    cin >> str;
+
+    result = "USE MASTER"
+             "\nGO"
+             "\nDROP DATABASE " + str + "\n";
+
+    c << result;
+    cout << "Fisierul selete.sql se afla la locatia: ./Results/ \n";
+
+}
---
+++ header.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include "create.h"
+#include "delete.h"
+using namespace std;
